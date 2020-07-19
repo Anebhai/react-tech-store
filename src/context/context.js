537e7d2d1847bf7f2c79f68505a8af2b0ae47e83
Anees Iqbal <ane.iqbal@gmail.com>
2020-07-19 18:50:17 +0530

    contentful data,environment variables

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { linkData } from './linkData';
 import { socialData } from './socialData';
-import { items } from './productData';
+// import { items } from './productData';
+import { client } from './contentful';
 
 const ProductContext = React.createContext();
 //Provider
@@ -31,7 +32,13 @@ class ProductProvider extends Component {
   };
 
   componentDidMount = () => {
-    this.setProducts(items);
+    // this.setProducts(items);
+    client
+      .getEntries({
+        content_type: 'techStoreProducts',
+      })
+      .then((response) => this.setProducts(response.items))
+      .catch(console.error);
   };
 
   setProducts = (products) => {
@@ -260,6 +267,7 @@ class ProductProvider extends Component {
         if (tempSearch === tempTitle) {
           return item;
         }
+        return null;
       });
     }
     this.setState({ filteredProducts: tempProducts });
